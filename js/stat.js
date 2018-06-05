c8d117c9fcef2929d8ae07ad4a98bd81ceef082a
SempreMai <lyuba.kudrina@gmail.com>
2018-06-05 13:30:49 +0200

    Rounded time in getMaxTime cleaned console logs and moved variables in the beginning

--- js/stat.js
+++ js/stat.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-undef */
 'use strict';
 
 var CLOUD_WIDTH = 420;
@@ -12,6 +11,8 @@ var BAR_HEIGHT = 150;
 var BAR_GAP = 50;
 
 var randomColor = 0;
+var maxElement = 0;
+var maxTime = 0;
 
 
 var renderCloud = function (ctx, x, y, color) {
@@ -20,7 +21,7 @@ var renderCloud = function (ctx, x, y, color) {
 };
 
 var getMaxElement = function (arr) {
-  var maxElement = arr[0];
+  maxElement = arr[0];
 
   for (var i = 0; i < arr.length; i++) {
     if (arr[i] > maxElement) {
@@ -28,30 +29,26 @@ var getMaxElement = function (arr) {
     }
   }
 
-  return maxElement;
+  return Math.floor(maxElement);
 };
 
 window.renderStatistics = function (ctx, players, times) {
-  console.log(players, times);
-  console.log(players[i]);
-
   renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
   renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
 
   ctx.fillStyle = '#000';
   ctx.font = '16px PT MONO';
   ctx.textBaseline = 'top';
-  ctx.fillText('Ура вы победили!', CLOUD_X + GAP, GAP);
-  ctx.fillText('Список результатов:', CLOUD_X + GAP, MESSAGE_HEIGHT + GAP);
+  ctx.fillText('Ура вы победили!', CLOUD_X + GAP, GAP * 2);
+  ctx.fillText('Список результатов:', CLOUD_X + GAP, MESSAGE_HEIGHT + GAP * 2);
 
-  var maxTime = getMaxElement(times);
+  maxTime = getMaxElement(times);
 
   for (var i = 0; i < players.length; i++) {
     ctx.fillStyle = '#000';
     ctx.textBaseline = 'bottom';
     ctx.fillText(players[i], CLOUD_X + BAR_GAP + (BAR_GAP + BAR_WIDTH) * i, CLOUD_Y + CLOUD_HEIGHT - GAP);
     randomColor = 'rgba' + '(' + '15, ' + '82, ' + '186, ' + String(0.3 + i / 10) + ')';
-    console.log(randomColor);
     ctx.fillStyle = (players[i] === 'Вы') ? 'rgba(255, 0, 0, 1)' : randomColor;
     ctx.fillRect(CLOUD_X + BAR_GAP + (BAR_GAP + BAR_WIDTH) * i, CLOUD_Y + 2 * MESSAGE_HEIGHT + 2 * GAP + (BAR_HEIGHT - (BAR_HEIGHT * times[i]) / maxTime), BAR_WIDTH, (BAR_HEIGHT * times[i]) / maxTime);
   }
