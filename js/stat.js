a0c6251f0181f5d4bd6f6fd99a00fd622b8d0a4c
SempreMai <lyuba.kudrina@gmail.com>
2018-06-05 19:15:48 +0200

    Changed getColor function, added x parameter

--- js/stat.js
+++ js/stat.js
@@ -40,10 +40,10 @@ var getRandomArbitrary = function (min, max) {
   return Math.round(Math.random() * (max - min) + min);
 };
 
-var getColor = function () {
-  return 'rgb(19, 48, ' + String(getRandomArbitrary(50, 160)) + ')';
+var getColor = function (x) {
+  return 'rgb(8, 133, ' + String(getRandomArbitrary(150, 250) + x) + ')';
 };
-
+console.log(getColor());
 window.renderStatistics = function (ctx, players, times) {
   renderCloud(ctx, CLOUD_X + CLOUD_GAP, CLOUD_Y + CLOUD_GAP, 'rgba(0, 0, 0, 0.7)');
   renderCloud(ctx, CLOUD_X, CLOUD_Y, WHITE_COLOR);
@@ -55,9 +55,8 @@ window.renderStatistics = function (ctx, players, times) {
   ctx.fillText(SECOND_MESSAGE_PHRASE, CLOUD_X + TEXT_GAP, TEXT_HEIGHT + TEXT_GAP * 3);
 
   var maxTime = getMaxElement(times);
-  var randomColor = getColor();
-
   for (var i = 0; i < players.length; i++) {
+    var randomColor = getColor(i);
     ctx.fillStyle = BLACK_COLOR;
     ctx.textBaseline = TEXT_BOTTOM;
     ctx.fillText(Math.floor(times[i]), CLOUD_X + BAR_GAP + (BAR_GAP + BAR_WIDTH) * i, CLOUD_Y + TEXT_HEIGHT * 2 + TEXT_GAP * 5 + (BAR_HEIGHT - (BAR_HEIGHT * times[i]) / maxTime));
