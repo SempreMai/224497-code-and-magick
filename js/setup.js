633fd1e0d9349d6d6241065c6ece0867c162f9bb
SempreMai <lyuba.kudrina@gmail.com>
2018-06-07 13:02:16 +0200

    Created random values for names, coats&eyes colors, added them in functions to create a wizard copy

--- js/setup.js
+++ js/setup.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var WIZARDS_NAMES = [
+var WIZARD_NAMES = [
   'Иван',
   'Хуан Себастьян',
   'Мария',
@@ -11,7 +11,7 @@ var WIZARDS_NAMES = [
   'Вашингтон'
 ];
 
-var WIZARS_SURNAMES = [
+var WIZARD_SURNAMES = [
   'да Марья',
   'Верон',
   'Мирабелла',
@@ -22,7 +22,7 @@ var WIZARS_SURNAMES = [
   'Ирвинг'
 ];
 
-var WIZARDS_COAT_COLORS = [
+var WIZARD_COAT_COLORS = [
   'rgb(101, 137, 164)',
   'rgb(241, 43, 107)',
   'rgb(146, 100, 161)',
@@ -31,7 +31,7 @@ var WIZARDS_COAT_COLORS = [
   'rgb(0, 0, 0)'
 ];
 
-var WIZARDS_EYES_COLORS = [
+var WIZARD_EYES_COLORS = [
   'black',
   'red',
   'blue',
@@ -48,23 +48,52 @@ var similarWizardTemplate = document.querySelector('#similar-wizard-template')
   .content
   .querySelector('.setup-similar-item');
 
+function clamp(value, min, max) {
+  return Math.min(Math.max(value, min), max);
+}
+
+var getRandomWizardName = function () {
+  var nameArrLength = WIZARD_NAMES.length - 1;
+  var surnameArrLength = WIZARD_SURNAMES.length - 1;
+  var randomWizardName = clamp(Math.random().toFixed(0), 0, nameArrLength);
+  var randomWizardSurname = clamp(Math.random().toFixed(0), 0, surnameArrLength);
+  return WIZARD_NAMES[randomWizardName] + ' ' + WIZARD_SURNAMES[randomWizardSurname];
+};
+
+var getRandomCoatColor = function () {
+  var wizardCoatsArrLength = WIZARD_COAT_COLORS.length - 1;
+  var randomWizardCoatColor = clamp(Math.random().toFixed(0), 0, wizardCoatsArrLength);
+  return WIZARD_COAT_COLORS[randomWizardCoatColor];
+};
+
+var getRandomEyesColor = function () {
+  var wizardEyesArrLength = WIZARD_EYES_COLORS.length - 1;
+  var randomWizardEyesColor = clamp(Math.random().toFixed(0), 0, wizardEyesArrLength);
+  return WIZARD_EYES_COLORS[randomWizardEyesColor];
+};
+
+console.log(getRandomEyesColor());
 
 var wizards = [
   {
-    name: WIZARD_NAMES[0],
-    coatColor: 'rgb(241, 43, 107)'
+    name: getRandomWizardName(),
+    coatColor: getRandomCoatColor(),
+    eyesColor: getRandomEyesColor()
   },
   {
-    name: WIZARD_NAMES[1],
-    coatColor: 'rgb(215, 210, 55)'
+    name: getRandomWizardName(),
+    coatColor: getRandomCoatColor(),
+    eyesColor: getRandomEyesColor()
   },
   {
-    name: WIZARD_NAMES[2],
-    coatColor: 'rgb(101, 137, 164)'
+    name: getRandomWizardName(),
+    coatColor: getRandomCoatColor(),
+    eyesColor: getRandomEyesColor()
   },
   {
-    name: WIZARD_NAMES[3],
-    coatColor: 'rgb(127, 127, 127)'
+    name: getRandomWizardName(),
+    coatColor: getRandomCoatColor(),
+    eyesColor: getRandomEyesColor()
   }
 ];
 
@@ -74,6 +103,7 @@ var renderWizard = function (wizard) {
 
   wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
   wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
+  wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
 
   return wizardElement;
 };
