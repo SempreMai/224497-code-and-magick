d83111fca5635c02a04abb37ed4a78d688c0874f
SempreMai <lyuba.kudrina@gmail.com>
2018-06-07 09:53:32 +0200

    Modified constants' and variables' names, added clump function, changed randomBlue function

--- js/stat.js
+++ js/stat.js
@@ -15,11 +15,11 @@ var CLOUD_TEXT_TOP_POSITION = 'top';
 var CLOUD_TEXT_COLOR = '#000';
 var CLOUD_FIRST_MESSAGE_PHRASE = 'Ура вы победили!';
 var CLOUD_SECOND_MESSAGE_PHRASE = 'Список результатов:';
-var STAT_BAR_WIDTH = 40;
-var MAX_BAR_HEIGHT = 150;
-var STAT_BAR_GAP = 50;
+var STAT_COLUMN_WIDTH = 40;
+var MAX_COLUMN_HEIGHT = 150;
+var STAT_COLUMN_GAP = 50;
 var PLAYER_NAME = 'Вы';
-var PLAYER_BAR_COLOR = 'rgba(255, 0, 0, 1)';
+var PLAYER_COLUMN_COLOR = 'rgba(255, 0, 0, 1)';
 
 var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
@@ -37,15 +37,13 @@ var getMaxElement = function (arr) {
   return maxElement;
 };
 
-function precise(x) {
-  return Number.parseFloat(x).toPrecision(1);
+function clamp(value, min, max) {
+  return Math.min(Math.max(value, min), max);
 }
 
+
 var getRandomBlueColor = function () {
-  var random = function (min, max) {
-    return Math.random() * (max - min) + min;
-  };
-  return 'rgba(0, 0, 250, ' + precise(random(0.2, 0.9)) + ')';
+  return 'rgba(0, 0, 250, ' + clamp(Math.random().toFixed(2), 0.2, 0.9) + ')';
 };
 
 var renderText = function (ctx, color, font, baseline, text, x, y) {
@@ -55,7 +53,7 @@ var renderText = function (ctx, color, font, baseline, text, x, y) {
   ctx.fillText(text, x, y);
 };
 
-var renderStatBar = function (ctx, color, x, y, width, height) {
+var renderStatColumn = function (ctx, color, x, y, width, height) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, width, height);
 };
@@ -64,25 +62,25 @@ window.renderStatistics = function (ctx, players, times) {
   renderCloud(ctx, CLOUD_X + CLOUD_SHADOW_GAP, CLOUD_Y + CLOUD_SHADOW_GAP, CLOUD_SHADOW_COLOR);
   renderCloud(ctx, CLOUD_X, CLOUD_Y, CLOUD_COLOR);
 
-  var MessageX = CLOUD_X + CLOUD_TEXT_GAP;
+  var messageX = CLOUD_X + CLOUD_TEXT_GAP;
   var firstMessageY = CLOUD_TEXT_GAP * 3;
   var secondMessageY = CLOUD_TEXT_HEIGHT + CLOUD_TEXT_GAP * 3;
-  renderText(ctx, CLOUD_TEXT_COLOR, CLOUD_TEXT_FONT, CLOUD_TEXT_TOP_POSITION, CLOUD_FIRST_MESSAGE_PHRASE, MessageX, firstMessageY);
-  renderText(ctx, CLOUD_TEXT_COLOR, CLOUD_TEXT_FONT, CLOUD_TEXT_TOP_POSITION, CLOUD_SECOND_MESSAGE_PHRASE, MessageX, secondMessageY);
+  renderText(ctx, CLOUD_TEXT_COLOR, CLOUD_TEXT_FONT, CLOUD_TEXT_TOP_POSITION, CLOUD_FIRST_MESSAGE_PHRASE, messageX, firstMessageY);
+  renderText(ctx, CLOUD_TEXT_COLOR, CLOUD_TEXT_FONT, CLOUD_TEXT_TOP_POSITION, CLOUD_SECOND_MESSAGE_PHRASE, messageX, secondMessageY);
 
   var maxTime = getMaxElement(times);
 
   for (var i = 0; i < players.length; i++) {
-    var randomBarColor = getRandomBlueColor();
-    var playerColor = (players[i] === PLAYER_NAME) ? PLAYER_BAR_COLOR : randomBarColor;
-    var resultsStat = Math.floor(times[i]);
-    var playersNames = players[i];
-    var statisticsX = CLOUD_X + STAT_BAR_GAP + (STAT_BAR_GAP + STAT_BAR_WIDTH) * i;
-    var statBarHeight = (MAX_BAR_HEIGHT * times[i]) / maxTime;
-    var playersResultsY = CLOUD_Y + CLOUD_TEXT_HEIGHT * 2 + CLOUD_TEXT_GAP * 5 + MAX_BAR_HEIGHT - statBarHeight;
-    var playersNamesY = CLOUD_Y + CLOUD_TEXT_HEIGHT * 3 + CLOUD_TEXT_GAP * 4 + MAX_BAR_HEIGHT - statBarHeight;
-    renderText(ctx, CLOUD_TEXT_COLOR, CLOUD_TEXT_FONT, CLOUD_TEXT_BOTTOM_POSITION, resultsStat, statisticsX, playersResultsY);
-    renderText(ctx, CLOUD_TEXT_COLOR, CLOUD_TEXT_FONT, CLOUD_TEXT_BOTTOM_POSITION, playersNames, statisticsX, CLOUD_Y + CLOUD_HEIGHT - CLOUD_TEXT_GAP);
-    renderStatBar(ctx, playerColor, statisticsX, playersNamesY, STAT_BAR_WIDTH, statBarHeight);
+    var randomColumnColor = getRandomBlueColor();
+    var playerColor = (players[i] === PLAYER_NAME) ? PLAYER_COLUMN_COLOR : randomColumnColor;
+    var playerTime = Math.floor(times[i]);
+    var statisticX = CLOUD_X + STAT_COLUMN_GAP + (STAT_COLUMN_GAP + STAT_COLUMN_WIDTH) * i;
+    var statColumnHeight = (MAX_COLUMN_HEIGHT * times[i]) / maxTime;
+    var playerTimeY = CLOUD_Y + CLOUD_TEXT_HEIGHT * 2 + CLOUD_TEXT_GAP * 5 + MAX_COLUMN_HEIGHT - statColumnHeight;
+    var playerNameY = CLOUD_Y + CLOUD_HEIGHT - CLOUD_TEXT_GAP;
+    var statColumnY = CLOUD_Y + CLOUD_TEXT_HEIGHT * 3 + CLOUD_TEXT_GAP * 4 + MAX_COLUMN_HEIGHT - statColumnHeight;
+    renderText(ctx, CLOUD_TEXT_COLOR, CLOUD_TEXT_FONT, CLOUD_TEXT_BOTTOM_POSITION, playerTime, statisticX, playerTimeY);
+    renderText(ctx, CLOUD_TEXT_COLOR, CLOUD_TEXT_FONT, CLOUD_TEXT_BOTTOM_POSITION, players[i], statisticX, playerNameY);
+    renderStatColumn(ctx, playerColor, statisticX, statColumnY, STAT_COLUMN_WIDTH, statColumnHeight);
   }
 };
